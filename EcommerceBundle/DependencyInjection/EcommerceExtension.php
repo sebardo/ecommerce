d356113557a30673aea571a0896682306dc31ce1
Sebastian2 <sebastian@gmail.com>
2016-08-04 16:51:01 +0200

    Add braintree class

--- EcommerceBundle/DependencyInjection/EcommerceExtension.php
+++ EcommerceBundle/DependencyInjection/EcommerceExtension.php
@@ -6,6 +6,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Braintree_Configuration;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -20,24 +21,25 @@ class EcommerceExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
-
+        $config = $this->processConfiguration($configuration, $configs);
+        
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
         
         if (isset($config['environment'])) {
-            $container->setParameter('comet_cult_braintree.environment', $config['environment']);
+            $container->setParameter('braintree.environment', $config['environment']);
             Braintree_Configuration::environment($config['environment']);
         }
         if (isset($config['merchant_id'])) {
-            $container->setParameter('comet_cult_braintree.merchant_id', $config['merchant_id']);
+            $container->setParameter('braintree.merchant_id', $config['merchant_id']);
             Braintree_Configuration::merchantId($config['merchant_id']);
         }
         if (isset($config['public_key'])) {
-            $container->setParameter('comet_cult_braintree.public_key', $config['public_key']);
+            $container->setParameter('braintree.public_key', $config['public_key']);
             Braintree_Configuration::publicKey($config['public_key']);
         }
         if (isset($config['private_key'])) {
-            $container->setParameter('comet_cult_braintree.private_key', $config['private_key']);
+            $container->setParameter('braintree.private_key', $config['private_key']);
             Braintree_Configuration::privateKey($config['private_key']);
         }
     }
